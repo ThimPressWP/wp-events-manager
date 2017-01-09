fa6cfdc0299e9b87ca0b1625cbcac78b3d87294e
haihld_58 <hoangleenhan@gmail.com>
2017-01-09 09:07:58 +0700

    Fix update event booking metabox

--- inc/admin/class-event-admin-metaboxes.php
+++ inc/admin/class-event-admin-metaboxes.php
@@ -12,7 +12,8 @@ class TP_Event_Admin_Metaboxes {
 		/**
 		 * Save post meta
 		 */
-		add_action( 'event_process_update_tp_event_meta', array( 'TP_Event_Admin_Metabox_Event', 'save' ), 10, 2 );
+		add_action( 'tp_event_process_update_tp_event_meta', array( 'TP_Event_Admin_Metabox_Event', 'save' ), 10, 2 );
+		add_action( 'tp_event_process_update_event_auth_book_meta', array( 'TP_Event_Admin_Metabox_Booking', 'save' ), 10, 2 );
 	}
 
 	/**
@@ -46,7 +47,7 @@ class TP_Event_Admin_Metaboxes {
 
 		if ( $post_type == 'tp_event' && ( empty( $_POST['event-nonce'] ) || !wp_verify_nonce( $_POST['event-nonce'], 'event_nonce' ) ) ) {
 			return false;
-		} elseif ( $post_type == 'event_auth_book' && ( empty( $_POST['event-booking-nonce'] ) || !wp_verify_nonce( $_POST['event-booking-nonce'], 'event-booking-nonce' ) ) ) {
+		} elseif ( $post_type == 'event_auth_book' && ( empty( $_POST['event-booking-nonce'] ) || !wp_verify_nonce( $_POST['event-booking-nonce'], 'event_booking_nonce' ) ) ) {
 			return false;
 		}
 
--- inc/admin/metaboxes/class-event-admin-metabox-booking.php
+++ inc/admin/metaboxes/class-event-admin-metabox-booking.php
@@ -5,7 +5,7 @@ class TP_Event_Admin_Metabox_Booking {
 
 	public static function save( $post_id, $post, $update ) {
 		if ( !empty( $_POST['_auth_status'] ) ) {
-			remove_action( 'tp_event_process_update_event_booking_meta', array( __CLASS__, 'save' ), 10, 3 );
+			remove_action( 'tp_event_process_update_event_auth_book_meta', array( __CLASS__, 'save' ), 10, 3 );
 			$booking = TP_Event_Booking::instance( $post_id );
 
 			$status = sanitize_text_field( $_POST['_auth_status'] );
@@ -49,7 +49,8 @@ class TP_Event_Admin_Metabox_Booking {
                     </select>
                 </td>
             </tr>
-			<?php wp_nonce_field( 'event_booking_nonce', 'event_booking_nonce' ); ?>
+			<?php wp_nonce_field( 'event_booking_nonce', 'event-booking-nonce' ); ?>
+			<?php wp_nonce_field( 'event_nonce', 'event-nonce' ); ?>
             </tbody>
         </table>
 		<?php
