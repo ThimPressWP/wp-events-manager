31f85e7bb7aeb3d2be2c367338de6d656d2aa54a
haihld_58 <hoangleenhan@gmail.com>
2017-01-10 09:43:41 +0700

    Update spacing

--- templates/loop/excerpt.php
+++ templates/loop/excerpt.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<div class="entry-content">
+<div class="event-content">
 	<?php the_excerpt(); ?>
 	<a class="tp_event_view-detail view-detail" href="<?php echo esc_attr( get_the_permalink() ); ?>">
 	<?php printf( '%s', __( 'View Detail', 'tp-event' ) ) ?>
