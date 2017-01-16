00dc0f588096900df5e9f4ca8ae2b8f13bdd4a0c
haihld_58 <hoangleenhan@gmail.com>
2017-01-16 16:56:47 +0700

    Update event countdown shortcode

--- templates/shortcodes/event-countdown.php
+++ templates/shortcodes/event-countdown.php
@@ -8,20 +8,24 @@
  */
 defined( 'ABSPATH' ) || exit;
 
-if ( !$args['event_id'] || !get_post( $args['event_id'] ) ) {
-	echo esc_html__( 'Invalid Event ID', 'tp-event' );
+if ( !$args['event_id'] || !get_post( $args['event_id'] ) ) { ?>
+    <p class="tp-event-notice error"><?php echo esc_html__( 'Invalid Event ID', 'tp-event' ); ?></p>
+	<?php
 } else {
 	$event = get_post( $args['event_id'] );
 	echo get_the_title( $args['event_id'] );
 
-	$time = tp_event_get_time( 'M j, Y H:i:s O', $event, false );
-	$date = new DateTime( date( 'Y-m-d H:i:s', strtotime( $time ) ), new DateTimeZone( tp_event_get_timezone_string() ) );
-	?>
+	$current_time = date( 'Y-m-d H:i' );
+	$time         = tp_event_get_time( 'Y-m-d H:i', $event, false ); ?>
     <div class="event-countdown">
 
-        <div class="tp_event_counter" data-time="<?php echo esc_attr( $date->format( 'M j, Y H:i:s O' ) ) ?>"></div>
+		<?php if ( $time > $current_time ) { ?>
+			<?php $date = new DateTime( date( 'Y-m-d H:i', strtotime( $time ) ), new DateTimeZone( tp_event_get_timezone_string() ) ); ?>
+            <div class="tp_event_counter" data-time="<?php echo esc_attr( $date->format( 'M j, Y H:i:s O' ) ) ?>"></div>
+		<?php } else { ?>
+            <p class="tp-event-notice error"><?php echo esc_html__( 'This event has expired', 'tp-event' ); ?></p>
+		<?php } ?>
 
     </div>
-	<?php
-}
+<?php } ?>
 
