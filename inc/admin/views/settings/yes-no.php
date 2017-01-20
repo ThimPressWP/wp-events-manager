bf8ca26f789267d79a699746b96446be8a4574fa
haihld_58 <hoangleenhan@gmail.com>
2017-01-20 13:53:32 +0700

    Check disable register event feature

--- inc/admin/views/settings/yes-no.php
+++ inc/admin/views/settings/yes-no.php
@@ -21,7 +21,7 @@ $val = tp_event_get_option( $field['id'] );
     <td class="event-form-field event-form-field-<?php echo esc_attr( $field['type'] ) ?>">
         <input type="hidden" name="<?php echo isset( $field['id'] ) ? esc_attr( $field['id'] ) : '' ?>" value="no" />
         <input type="checkbox" name="<?php echo isset( $field['id'] ) ? esc_attr( $field['id'] ) : '' ?>" value="yes"
-			<?php echo $custom_attr ?><?php echo empty( $val ) ? checked( $field['default'], 'yes' ) : checked( $val, 'yes', false ); ?>
+			<?php echo empty( $val ) ? checked( $field['default'], 'yes' ) : checked( $val, 'yes', false ); ?>
         />
 
 		<?php if ( isset( $field['desc'] ) ) : ?>
