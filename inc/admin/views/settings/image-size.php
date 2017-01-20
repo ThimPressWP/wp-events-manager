4b168d8aba4b40fc5a32db25ff1a60814ce63e3f
haihld_58 <hoangleenhan@gmail.com>
2017-01-20 11:11:34 +0700

    Fix setting fields

--- inc/admin/views/settings/image-size.php
+++ inc/admin/views/settings/image-size.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 $width  = tp_event_get_option( $field['id'] . '_width', isset( $field['default']['width'] ) ? $field['default']['width'] : 270 );
 $height = tp_event_get_option( $field['id'] . '_height', isset( $field['default']['height'] ) ? $field['default']['height'] : 270 );
 ?>
-<tr valign="top" <?php echo isset( $field['class'] ) ? 'class="' . $field['class'] . '"' : ''; ?>>
+<tr valign="top" <?php echo $field['class'] ? 'class="' . $field['class'] . '"' : ''; ?>>
     <th scope="row">
 		<?php if ( isset( $field['title'] ) ) : ?>
             <label for="<?php echo isset( $field['id'] ) ? esc_attr( $field['id'] ) : '' ?>">
