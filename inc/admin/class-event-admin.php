35db7ce15c586d04d0cc0f400ca2003459a045f9
haihld_58 <hoangleenhan@gmail.com>
2017-02-20 11:11:01 +0700

    Fix duplicate plugin instance

--- inc/admin/class-event-admin.php
+++ inc/admin/class-event-admin.php
@@ -4,17 +4,17 @@ defined( 'ABSPATH' ) || exit();
 
 class TP_Event_Admin {
 
-    public function __construct() {
+	public function __construct() {
 
-        $this->_includes();
-    }
+		$this->_includes();
+	}
 
-    private function _includes() {
-        TP_Event::instance()->_include( 'inc/admin/class-event-admin-menu.php' );
-        TP_Event::instance()->_include( 'inc/admin/class-event-admin-assets.php' );
-        TP_Event::instance()->_include( 'inc/admin/class-event-admin-metaboxes.php' );
-		TP_Event::instance()->_include( 'inc/admin/class-event-admin-settings.php' );
-		TP_Event::instance()->_include( 'inc/admin/class-event-admin-users.php' );
+	private function _includes() {
+		include( WP_EVENT_PATH . 'inc/admin/class-event-admin-menu.php' );
+		include( WP_EVENT_PATH . 'inc/admin/class-event-admin-assets.php' );
+		include( WP_EVENT_PATH . 'inc/admin/class-event-admin-metaboxes.php' );
+		include( WP_EVENT_PATH . 'inc/admin/class-event-admin-settings.php' );
+		include( WP_EVENT_PATH . 'inc/admin/class-event-admin-users.php' );
 	}
 
 }
