d68e58903124764fa01f27afe7663f9311757668
Tần <doantan1802@gmail.com>
2023-10-04 09:15:24 +0700

    upload 04/10/2023

--- inc/Model/WpemsEventModel.php
+++ inc/Model/WpemsEventModel.php
@@ -10,63 +10,64 @@ class WpemsEventModel {
         $this->database = WpemsEventDatabase::get_instance();
     }
 
-	public function getEventTitle( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-		return $event_data->post_title;
+	private function fetchEventData( $event_id ) {
+        $event_data = $this->database->getEventData( $event_id );
+        return $event_data;
 	}
 
+	private function getEventDataValue( $event_id, $property ) {
+		$event_data = $this->fetchEventData( $event_id );
+		return $event_data->$property;
+	}
+
+	public function getEventTitle( $event_id ) {
+		return $this->getEventDataValue( $event_id, 'post_title' );
+	}
+
+	/**
+	 * guid stores thumbnail path
+	 */
 	public function getEventThumbnail( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-		return $event_data->guid;
+		return $this->getEventDataValue( $event_id, 'guid' );
 	}
 
 	public function getEventContent( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-		return $event_data->post_content;
+		return $this->getEventDataValue( $event_id, 'post_content' );
 	}
 
 	public function getEventStartTime( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-		return $event_data->tp_event_time_start;
+		return $this->getEventDataValue( $event_id, 'tp_event_time_start' );
 	}
 
 	public function getEventEndTime( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-	    return $event_data->tp_event_time_end;
+		return $this->getEventDataValue( $event_id, 'tp_event_time_end' );
 	}
 
 	public function getEventStartDate( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-	    return $event_data->tp_event_date_start;
+		return $this->getEventDataValue( $event_id, 'tp_event_date_start' );
 	}
 
 	public function getEventEndDate( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-	    return $event_data->tp_event_date_end;
+		return $this->getEventDataValue( $event_id, 'tp_event_date_end' );
 	}
 
 	public function getEventRegisterEndTime( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-	    return $event_data->tp_event_registration_end_time;
+		return $this->getEventDataValue( $event_id, 'tp_event_registration_end_time' );
 	}
 
 	public function getEventRegisterEndDate( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-	    return $event_data->tp_event_registration_end_date;
+		return $this->getEventDataValue( $event_id, 'tp_event_registration_end_date' );
 	}
 
 	public function getEventLocationF( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-	    return $event_data->tp_event_location_iframe;
+		return $this->getEventDataValue( $event_id, 'tp_event_location_iframe' );
 	}
 
 	public function getEventIframe( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-		return $event_data->tp_event_iframe;
+		return $this->getEventDataValue( $event_id, 'tp_event_iframe' );
 	}
 
 	public function getEventSchedules( $event_id ) {
-		$event_data = $this->database->getEventData( $event_id );
-		return $event_data->tp_event_schedules;
+		return $this->getEventDataValue( $event_id, 'tp_event_schedules' );
 	}
 }
